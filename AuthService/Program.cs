eb1a36096851cbd2c683a7f71f5ee822908157c9
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2025-01-26 22:39:08 +0100

    fix: Enable CORS using cors middleware

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,4 +1,3 @@
-
 using System.Text;
 using AuthService.Config;
 using AuthService.Data;
@@ -7,7 +6,6 @@ using AuthService.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
 
 namespace AuthService
 {
@@ -39,26 +37,35 @@ namespace AuthService
             builder.Services.AddScoped<UserService>();
             builder.Services.AddScoped<JwtService>();
 
-            // Add services to the container.
+            // Add CORS service
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReactNativeApp", builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:8081") // Add your React Native app's URL
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            // Use CORS middleware
+            app.UseCors("AllowReactNativeApp");
+
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
             app.MapControllers();
 
             app.Run();
