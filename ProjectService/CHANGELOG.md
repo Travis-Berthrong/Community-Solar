275921dff56c337913d53766acc8f4150e01b579
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2025-01-26 17:57:15 +0100

    feat: setup service structure

---
+++ ProjectService/CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.0.1 (2025-01-22)
+
+
+### Features
+
+* defined user datamodel, setup express ([4559370](https://github.com/Travis-Berthrong/Community-Solar/commit/4559370595b13515d79982c4075c8a4a9d57764d))
+
+
+
---
+++ ProjectService/src/index.ts
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import mongoose from "mongoose";
+import app from "./app";
+
+config();
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/project-service';
+const startApp = async (): Promise<void> => {
+    try {
+        mongoose.connect(`${MONGO_URI}`, {
+            serverSelectionTimeoutMS: 5000
+          }).then(
+            () => {
+                console.log('Connected to Mongodb');
+                mongoose.connection.on('error', function (err) {
+                    console.error(`Mongoose error: ${err}`)
+                });
+             },
+            err => { console.error(`Error connecting to Mongodb: ${err}`) }
+        );
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}/api`);
+        }).on('error', (error) => {
+            console.error(`Server encountered an error: ${error.message}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+startApp();
+export default startApp;
