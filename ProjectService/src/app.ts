abbc40ce8570e06d2b4a25f1d74ff39cbaedb023
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2025-01-26 19:37:48 +0100

    feat: Implemented project controller

--- ProjectService/src/app.ts
+++ ProjectService/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { ProjectController } from "./controllers/ProjectController";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const projectController = new ProjectController();
+app.use("/api", projectController.router);
 
 export default app;
---
+++ ProjectService/src/controllers/ProjectController.ts
@@ -0,0 +1,112 @@
+import { Router, Request, Response } from 'express';
+import ProjectService from '../services/ProjectService';
+import { ObjectId } from 'bson';
+
+import { Logger } from "sitka";
+import { IProject } from 'src/datamodels/Project';
+
+export class ProjectController {
+    public path = '/projects';
+    public router = Router();
+    private readonly logger = Logger.getLogger({ name: 'ProjectController' });
+
+    constructor() {
+    this.initRoutes();
+    }
+
+    public initRoutes() {
+        this.router.get(this.path, this.getAllProjects);
+        this.router.get(`${this.path}/:id`, this.getProjectById);
+        this.router.post(this.path, this.createProject);
+        this.router.put(`${this.path}/:id`, this.updateProject);
+        this.router.delete(`${this.path}/:id`, this.deleteProject);
+    }
+
+    getAllProjects = async (_req: Request, res: Response) => {
+    try {
+        const projects = await ProjectService.getProjects();
+        res.send(projects);
+    } catch (error) {
+        this.logger.error(error);
+        res.status(500).send({ message: "Internal server error" });
+    }
+    };
+
+    getProjectById = async (req: Request, res: Response) => {
+        try {
+            let id: ObjectId;
+            try {
+                id = new ObjectId(req.params.id);
+            } catch (error) {
+                res.status(400).send({ message: 'Invalid project id' });
+            }
+            if (!id) {
+                return;
+            }
+            const project = await ProjectService.getProjectById(id);
+            if (!project) {
+                res.status(404).send({ message: 'Project not found' });
+                return;
+            }
+            res.send(project);
+        } catch (error) {
+            this.logger.error(error);
+            res.status(500).send({ message: "Internal server error" });
+        }
+    };
+
+    createProject = async (req: Request, res: Response) => {
+        try {
+            const project = req.body;
+            const newProject = await ProjectService.createProject(project);
+            res.status(201).send(newProject);
+        } catch (error) {
+            this.logger.error(error);
+            res.status(500).send({ message: "Internal server error" });
+        }
+    };
+
+    updateProject = async (req: Request, res: Response) => {
+        try {
+            let id: ObjectId;
+            try {
+                id = new ObjectId(req.params.id);
+            } catch (error) {
+                res.status(400).send({ message: 'Invalid project id' });
+            }
+            if (!id) {
+                return;
+            }
+            const project: IProject = req.body;
+            await ProjectService.updateProject(id, project);
+            res.send("Project updated successfully");
+        } catch (error) {
+            this.logger.error(error);
+            res.status(500).send({ message: "Internal server error" });
+        }
+    };
+
+    deleteProject = async (req: Request, res: Response) => {
+        try {
+            let id: ObjectId;
+            try {
+                id = new ObjectId(req.params.id);
+            } catch (error) {
+                res.status(400).send({ message: 'Invalid project id' });
+            }
+            if (!id) {
+                return;
+            }
+            const projectExists = await ProjectService.getProjectById(id);
+            if (!projectExists) {
+                res.status(404).send({ message: 'Project not found' });
+                return;
+            }
+            await ProjectService.deleteProject(id);
+            res.send("Project deleted successfully");
+        } catch (error) {
+            this.logger.error(error);
+            res.status(500).send({ message: "Internal server error" });
+        }
+    };
+}
